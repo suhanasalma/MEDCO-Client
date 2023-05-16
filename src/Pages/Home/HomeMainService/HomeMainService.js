3c81ab704fb853d4e26137b9124626fbd78f43cd
suhanasalma <108382046+suhanasalma@users.noreply.github.com>
2023-05-17 00:39:15 +0600

    main services responsived

--- src/Pages/Home/HomeMainService/HomeMainService.js
+++ src/Pages/Home/HomeMainService/HomeMainService.js
@@ -35,28 +35,12 @@ const HomeMainService = () => {
             details:'Now you can connect with the doctor from anywhere. There is no need to doubt the quality of consultation and treatment offered',
 
         },
-        {
-            name:"Visit Hospital",
-            image:hospital,
-            details:'Now you can connect with the doctor from anywhere. There is no need to doubt the quality of consultation and treatment offered'
-
-        },
-        {
-            name:"Book a Hotel",
-            image:hotel,
-            details:'Now you can connect with the doctor from anywhere. There is no need to doubt the quality of consultation and treatment offered',
-
-        },
+       
     ]
     return (
         <div className='my-20 w-10/12 mx-auto '>
             <Title title="Our Main Services" design='text-4xl text-center'/>
-           {/* <div className='flex gap-10 mt-10 '>
-           {
-                mainServices.map((card,i) => <Card key={i} details={card.details} title={card.name} imgDesign='w-20 mx-auto' CardDesign={`shadow-xl hover:bg-light-green space-y-5 p-4 h-96 flex flex-col justify-center rounded-lg hover:-translate-y-6 transition ease-in delay-75`} image={card.image} titleDesign='font-bold text-lg' detailsDesign='text-sm'/>)
-            }
-           </div> */}
-           <div className='container mt-10 mb-32'>
+           <div className='container mt-10 mb-32 xl:justify-between justify-center'>
             {
                 mainServices.map((card,i)=><div key={i} className='card'>
                     <div className='box'>
