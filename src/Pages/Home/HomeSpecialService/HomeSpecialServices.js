559fc0bafb41b3ca765164856b0811eb8de01fd1
suhana salma <salma@parallaxlogic.net>
2023-05-18 18:30:45 +0600

    working on it

--- src/Pages/Home/HomeSpecialService/HomeSpecialServices.js
+++ src/Pages/Home/HomeSpecialService/HomeSpecialServices.js
@@ -1,102 +1,74 @@
 import React, { useState } from "react";
 import Title from "../../../Components/Title/Title";
 import Details from "../../../Components/Details/Details";
-import drugs from "../../../Assests/specialService/hotel.png";
-import adhd from "../../../Assests/HomeCarousel/ADHD.jpg";
-import diabetics from "../../../Assests/HomeCarousel/diabetics.jpg";
-import pregnancy from "../../../Assests/HomeCarousel/pregnancy.jpg";
-import blood from "../../../Assests/HomeCarousel/blood.jpeg";
-import volunter from "../../../Assests/HomeCarousel/volunter.jpg";
-import ambulance from "../../../Assests/specialService/ambulance.jpg";
+import consultation from "../../../Assests/specialService/consultation.png";
 import hotel from "../../../Assests/specialService/hotel.png";
-import doctors from "../../../Assests/specialService/doctors.jpg";
+import doctors from "../../../Assests/specialService/doctors.png";
 import gift from "../../../Assests/specialService/gitCard.jpg";
-import consultaion from "../../../Assests/specialService/consultation.jpg";
-import covid from "../../../Assests/specialService/covid.png";
-import pharmacy from "../../../Assests/specialService/pharmacy.jpg";
-
+import period from "../../../Assests/specialService/period.jpg";
+import pharmacy from "../../../Assests/specialService/pharmacy.png";
 import "./SpecialServices.css";
 
-
 const HomeSpecialServices = () => {
-    const services = [
-      {
-        name: "",
-        img: drugs,
-        class: "one",
-        divDesign: "w-20 h-20 object-cover rounded-full",
-      },
-      {
-        name: "",
-        img: diabetics,
-        class: "two",
-        divDesign: "w-full h-full object-cover object-top",
-      },
-      {
-        name: "",
-        img: pregnancy,
-        class: "three",
-      },
-      {
-        name: "",
-        img: hotel,
-        class: "four",
-      },
-      {
-        name: "",
-        img: volunter,
-        class: "five",
-      },
-      {
-        name: "",
-        img: blood,
-        class: "six",
-      },
-      {
-        name: "",
-        img: volunter,
-        class: "seven",
-      },
-    ];
-    const [service,setService] = useState()
+  const specialServices = [
+    {
+      name: "Free Weekly Consultation",
+      image: consultation,
+      details:
+        "Now you can connect with the doctor from anywhere. There is no need to doubt the quality of consultation and treatment offered",
+    },
+    {
+      name: "Online Pharmacy",
+      image: pharmacy,
+      details:
+        "Now you can connect with the doctor from anywhere. There is no need to doubt the quality of consultation and treatment offered",
+    },
+    {
+      name: "Doctors Community",
+      image: doctors,
+      details:
+        "Now you can connect with the doctor from anywhere. There is no need to doubt the quality of consultation and treatment offered",
+    },
+    {
+      name: "Book a Hotel",
+      image: hotel,
+      details:
+        "Now you can connect with the doctor from anywhere. There is no need to doubt the quality of consultation and treatment offered",
+    },
+    {
+      name: "Gift Card",
+      image: gift,
+      details:
+        "Now you can connect with the doctor from anywhere. There is no need to doubt the quality of consultation and treatment offered",
+    },
+    {
+      name: "Menstrual Calender",
+      image: period,
+      details:
+        "Now you can connect with the doctor from anywhere. There is no need to doubt the quality of consultation and treatment offered",
+    },
+  ];
+
   return (
-    <section className="w-10/12 mx-auto mb-32">
-      <div className="space-y-5">
-        <Title title="Our Special Services" />
-        <Details
-          design="text-sm w-4/6"
-          details='Our patients are served by over 900 nurses and over 1,200 doctors and dentists, covering 55 sub-specialties. The Red Cross gives Bumrungrad its top classification of "A" – defined as "a tertiary care center which should be able to provide quality care in all medical specialties including invasive cardiology, cardiac and neurosurgery."'
-        />
+    <div className="my-20 w-10/12 mx-auto ">
+      <Title title="Our Main Services" design="text-4xl text-center" />
+      <div className="grid grid-cols-3 gap-10 justify-items-center content-center	">
+        {specialServices.map((card, i) => (
+          <div className="w-72 h-52 transition-all delay-150 hover:w-96 border-2" key={i}>
+                <img className="w-20 h-20 object-cover" src={card.image} alt="" />
+                <Title title={card.name} design="font-bold text-lg" />
+                {/* <Details details={card.details} design='text-sm'/> */}
+          </div>
+        ))}
       </div>
-      <article className="flex justify-between items-center gap-20">
-        <section className="w-2/6">
-          <img src={doctors} alt=""></img>
-        </section>
-        <section className="wrapper w-4/6 relative">
-          <div classNam="one ">
-            <img src={doctors} className="" alt=""></img>
-          </div>
-          <div className="two img">
-            <img src={ambulance} alt=""></img>
-          </div>
-          <div className="three">
-            <img src={gift} alt=""></img>
-          </div>
-          <div className="four img">
-            <img src={hotel} alt=""></img>
-          </div>
-          <div className="five">
-            <img src={pharmacy} alt=""></img>
-          </div>
-          <div className="six">
-            <img src={covid} alt=""></img>
-          </div>
-          <div className="seven">
-            <img src={consultaion} alt=""></img>
-          </div>
-        </section>
-      </article>
-    </section>
+      <div class="grid hover:grid-flow-row-dense grid-cols-3 grid-rows-3  gap-10">
+  <div class="hover:col-span-2 w-52 bg-green ">01</div>
+  <div class="hover:col-span-2  w-52 bg-green ">02</div>
+  <div class=" bg-green  w-52">03</div>
+  <div class=" bg-green  w-52">04</div>
+  <div class=" bg-green  w-52">05</div>
+</div>
+    </div>
   );
 };
 
