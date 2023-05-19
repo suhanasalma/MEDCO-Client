1263bb66a1ba1444d13efb98a6705aa00612842f
suhanasalma <108382046+suhanasalma@users.noreply.github.com>
2023-05-19 19:03:50 +0600

    view all reviews button added

--- src/Pages/Home/PatientFeedBacks/PatientFeedBack.js
+++ src/Pages/Home/PatientFeedBacks/PatientFeedBack.js
@@ -3,19 +3,23 @@ import girl1 from "../../../Assests/Reviews/girl1.jpg";
 import girl2 from "../../../Assests/Reviews/girl2.jpg";
 import boy1 from "../../../Assests/Reviews/boy1.jpg";
 import boy2 from "../../../Assests/Reviews/boy2.jpg";
+import { Link } from "react-router-dom";
+import './PatientFeedBack.css'
 
 const PatientFeedBack = () => {
   const reviews = [
     {
+      id: 0,
       name: "allison",
       age: "22",
       title: "Great Service",
       details:
-        "This was such a great experiance Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit aspernatur excepturi placeat! Ab hic maiores iure incidunt iste blanditiis molestias!",
+        "This was such a great experiance Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit aspernatur excepturi placeat! Ab hic maiores iure incidunt iste blanditiis molestias! This was such a great experiance Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit aspernatur excepturi placeat! Ab hic maiores iure incidunt iste blanditiis molestias!",
       img: girl1,
     },
     {
-      name: "allison",
+      id: 1,
+      name: "allison1",
       age: "22",
       title: "Great Service",
       details:
@@ -23,7 +27,8 @@ const PatientFeedBack = () => {
       img: girl2,
     },
     {
-      name: "allison",
+      id: 2,
+      name: "allison2",
       age: "22",
       title: "Great Service",
       details:
@@ -31,7 +36,8 @@ const PatientFeedBack = () => {
       img: boy1,
     },
     {
-      name: "allison",
+      id: 3,
+      name: "allison3",
       age: "22",
       title: "Great Service",
       details:
@@ -39,10 +45,27 @@ const PatientFeedBack = () => {
       img: boy2,
     },
   ];
-  const [review, setReview] = useState(reviews[0]);
+  const [selectedReviewIndex, setSelectedReviewIndex] = useState(0);
+  const selectedReview = reviews[selectedReviewIndex];
+
+  const handleNextSlide = () => {
+    setSelectedReviewIndex((prevIndex) =>
+      prevIndex === reviews.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const handlePreviousSlide = () => {
+    setSelectedReviewIndex((prevIndex) =>
+      prevIndex === 0 ? reviews.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleLowerSliderButton = (id) => {
+    setSelectedReviewIndex(id);
+  };
   return (
     <div className="w-11/12 lg:w-10/12 mx-auto sm:flex justify-between  my-32">
-      <section className="bg-light-green sm:w-1/2  p-10 space-y-20">
+      <Link className="bg-light-green sm:w-1/2  p-10 space-y-20 reviewCard">
         <div className="flex justify-between space-y-20">
           <img
             src={girl1}
@@ -56,7 +79,10 @@ const PatientFeedBack = () => {
           />
         </div>
         <p className="text-2xl font-bold text-center text-green">
-          What People are Saying About Us
+          What People are Saying About Us{" "}
+          <span className="hidden  px-4  border-b-2 border-gray text-gray">
+            View All
+          </span>
         </p>
         <div className="flex justify-between -space-y-10">
           <img
@@ -70,35 +96,49 @@ const PatientFeedBack = () => {
             className="w-12 h-12 object-cover rounded-full ring-offset-2 ring-4"
           />
         </div>
-      </section>
+      </Link>
       <section className="bg-light-gray sm:w-1/2 relative p-10">
         <div className="flex gap-10 absolute right-0  p-5">
-          <button className="ring-offset-2 ring-4 w-10 h-10 rounded-full">
+          <button
+            onClick={handlePreviousSlide}
+            className="ring-offset-2 ring-4 w-10 h-10 rounded-full text-green font-bold"
+          >
             ❮
           </button>
-          <button className="ring-offset-2 ring-4 w-10 h-10 rounded-full">
+          <button
+            onClick={handleNextSlide}
+            className="ring-offset-2 ring-4 w-10 h-10 rounded-full text-green font-bold"
+          >
             ❯
           </button>
         </div>
         <article className="mt-20 space-y-3">
-          <p className="font-bold text-green text-lg">{review.title}</p>
-          <p className="">{review.details}</p>
+          <p className="font-bold text-green text-lg">{selectedReview.title}</p>
+          <p className="">{selectedReview.details}</p>
           <div className="flex items-center gap-5">
             <img
               className="w-12 h-12 object-cover rounded-full ring-offset-2 ring-4 "
-              src={review.img}
+              src={selectedReview.img}
               alt=""
             />
             <div>
-              <p className="font-bold">{review.name}</p>
-              <p className="text-sm text-gray">{review.age} year's old</p>
+              <p className="font-bold">{selectedReview.name}</p>
+              <p className="text-sm text-gray">
+                {selectedReview.age} year's old
+              </p>
             </div>
           </div>
         </article>
         <div className="flex gap-3 absolute right-0 bottom-0 p-5 ">
-          {reviews.map((review) => (
+          {reviews.map((review, i) => (
             <img
-              className={`w-7 h-7 object-cover rounded-full cursor-pointer`}
+              onClick={() => handleLowerSliderButton(i)}
+              key={i}
+              className={`${
+                selectedReview.id === review.id
+                  ? "w-9 h-9 object-cover rounded-full cursor-pointer ring-offset-2 ring-4"
+                  : "w-7 h-7 object-cover rounded-full cursor-pointer"
+              } `}
               src={review.img}
               alt=""
             />
