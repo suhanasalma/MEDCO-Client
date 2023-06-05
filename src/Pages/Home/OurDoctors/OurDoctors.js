5548772d658bcfcdf0dff6a2ac0bbde019c59f4e
suhana salma <salma@parallaxlogic.net>
2023-06-05 16:24:30 +0600

    doctor card responsived

--- src/Pages/Home/OurDoctors/OurDoctors.js
+++ src/Pages/Home/OurDoctors/OurDoctors.js
@@ -27,80 +27,49 @@ const OurDoctors = () => {
   };
 
   return (
-    <div className="flex md:flex-row flex-col-reverse lg:gap-3 xl:gap-0 gap-8 flex-wrap lg:w-10/12 w-11/12 mx-auto md:justify-between justify-center items-center mt-10 mb-20 ">
-      <div className="space-y-8">
-        <div className="space-y-8 md:block hidden">
-        <p className="font-bold text-lg">Our Doctors</p>
+    <div className="lg:w-10/12 w-11/12 mx-auto mt-10 mb-20 border-4">
+      <div className="space-y-4 hidden mb-5 secondDetails">
+        <p className="font-bold text-lg">Our Doctors2</p>
         <Title design="text-4xl" title="Qualified Doctors" />
         <Details
-          design="w-80 lg:w-96"
+          // design="w-96"
           details="Handle directly by general doctors and professional and experienced specialist doctors"
         />
-        </div>
-        <div
-          style={{
-            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${selectedDoctor.bg})`,
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-          }}
-          className={`${selectedDoctor.imgClass} text-white bg border-2  h-64 w-80 sm:h-64 md:w-72 md:h-52 lg:w-96 lg:h-64 rounded-s-3xl rounded-br-3xl p-5 xl:p-10 space-y-5 mx-auto`}
-        >
-          <p className="lg:text-2xl">{selectedDoctor.department}</p>
-          <p className="text-xl lg:text-3xl font-bold leading-relaxed">
-            {selectedDoctor.name}
-          </p>
-          <p>
-            <Link>Read More </Link>
-          </p>
-        </div>
-        <div>
-          <LinkButton design='w-52 border-2 border-green' title="View All Doctors" />
-        </div>
-        <div className="flex gap-5 justify-center items-center mt-5  md:hidden ">
-            {ourDoctorsInfo.map((doctor) => (
-              <button
-                key={doctor.id}
-                onClick={() => handleLowerSliderButton(doctor.id)}
-                className={`${
-                  selectedDoctor.id === doctor.id ? "bg-green" : ""
-                } w-5 h-5 border-4 border-green rounded-full`}
-              ></button>
-            ))}
-          </div>
       </div>
-      
-      <div className="">
-      <div className="space-y-8 md:hidden block mb-10">
-        <p className="font-bold text-lg">Our Doctors</p>
-        <Title design="text-4xl" title="Qualified Doctors" />
-        <Details
-          design="w-96"
-          details="Handle directly by general doctors and professional and experienced specialist doctors"
-        />
-        </div>
-        <section className="relative">
-          <div className="border-[16px] border-brown w-[21rem] h-[22rem] lg:w-[24rem] lg:h-[24rem] xl:w-[30rem] xl:h-[30rem] rounded-xl  md:rounded-s-full overflow-hidden mx-auto">
-            <img
-              className={`${selectedDoctor.imgClass} w-full h-full object-cover object-top `}
-              src={selectedDoctor?.img}
-              alt=""
+      <div className="flex flex-row lg:gap-3 xl:gap-0 gap-8  md:justify-between justify-center items-center ourDocContainer">
+        <div className="space-y-5">
+          <div className="space-y-8 sm:block hidden doctorsHeader">
+            <p className="font-bold text-lg">Our Doctors</p>
+            <Title design="text-4xl" title="Qualified Doctors" />
+            <Details
+              design="w-80 lg:w-96"
+              details="Handle directly by general doctors and professional and experienced specialist doctors"
             />
           </div>
-
-          <button
-            onClick={handleNextSlide}
-            className="bg-brown rounded-full w-10 h-10 text-white font-bold text-2xl absolute top-52 left-10"
+          <div
+            style={{
+              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${selectedDoctor.bg})`,
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+            }}
+            className={`${selectedDoctor.imgClass} text-white bg border-2  h-64 w-72 sm:h-52 md:w-72 md:h-52 lg:w-96 lg:h-64 rounded-s-3xl rounded-br-3xl p-5 xl:p-10 space-y-5 doctorCard`}
           >
-            ❯
-          </button>
-          {/* <button
-              onClick={handlePreviousSlide}
-              className="bg-brown rounded-full w-10 h-10 text-white font-bold text-2xl absolute top-40 left-80"
-            >
-              ❮
-            </button> */}
-          <div className="md:flex gap-5 justify-end items-center mt-5 hidden">
+            <p className="lg:text-2xl">{selectedDoctor.department}</p>
+            <p className="text-xl lg:text-3xl font-bold leading-relaxed">
+              {selectedDoctor.name}
+            </p>
+            <p>
+              <Link>Read More </Link>
+            </p>
+          </div>
+          <div>
+            <LinkButton
+              design="w-52 border-2 border-green"
+              title="View All Doctors"
+            />
+          </div>
+          <div className="gap-2 justify-center items-center mt-5  hidden secondBtn">
             {ourDoctorsInfo.map((doctor) => (
               <button
                 key={doctor.id}
@@ -111,7 +80,37 @@ const OurDoctors = () => {
               ></button>
             ))}
           </div>
-        </section>
+        </div>
+
+        <div className="">
+          <section className="relative">
+            <div className="border-[16px] border-brown sm:w-[19rem] sm:h-[19rem] md:w-[21rem] md:h-[22rem] lg:w-[24rem] lg:h-[24rem] xl:w-[30rem] xl:h-[30rem] rounded-xl  sm:rounded-s-full overflow-hidden mx-auto doctorImage">
+              <img
+                className={`${selectedDoctor.imgClass} w-full h-full object-cover object-top `}
+                src={selectedDoctor?.img}
+                alt=""
+              />
+            </div>
+
+            <button
+              onClick={handleNextSlide}
+              className="bg-brown rounded-full w-10 h-10 text-white font-bold text-2xl absolute top-52 left-10 imgBtn"
+            >
+              ❯
+            </button>
+            <div className="flex gap-2 md:gap-5 justify-end items-center mt-5 firstBtn">
+              {ourDoctorsInfo.map((doctor) => (
+                <button
+                  key={doctor.id}
+                  onClick={() => handleLowerSliderButton(doctor.id)}
+                  className={`${
+                    selectedDoctor.id === doctor.id ? "bg-green" : ""
+                  } w-5 h-5 border-4 border-green rounded-full`}
+                ></button>
+              ))}
+            </div>
+          </section>
+        </div>
       </div>
     </div>
   );
