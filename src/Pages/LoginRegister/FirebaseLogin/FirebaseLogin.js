4ea83b0e79b379e66dcd316ddb155ad077060bda
suhana salma <salma@parallaxlogic.net>
2023-06-12 17:47:02 +0600

    working on it

--- src/Pages/LoginRegister/FirebaseLogin/FirebaseLogin.js
+++ src/Pages/LoginRegister/FirebaseLogin/FirebaseLogin.js
@@ -2,6 +2,7 @@ import React from "react";
 import { FaGooglePlusG, FaFacebookF, FaLinkedinIn } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { signInWithGoogleAsync } from "../../../StateManagment/FirebaseLoginSlice/FirebaseLoginSlice";
+import { Link } from "react-router-dom";
 
 const FirebaseLogin = () => {
   const dispatch = useDispatch();
@@ -11,17 +12,24 @@ const FirebaseLogin = () => {
   const handleSignInWithGoogle = () => {
     dispatch(signInWithGoogleAsync());
   };
+
+  const googleAuth = () => {
+		window.open(
+			`http://localhost:5000/auth/google/callback`
+		);
+	};
+
   return (
     <div className="flex gap-5 my-5 items-center justify-center">
       <div className="border-2 border-light-gray rounded-full p-2 text-green hover:scale-125 duration-1000 ease-in-out hover:border-green hover:text-brown">
         <FaFacebookF />
       </div>
-      <div
-        onClick={handleSignInWithGoogle}
+      <Link  onClick={googleAuth} 
+
         className="border-2 border-light-gray rounded-full p-2 text-green hover:scale-125 duration-1000 ease-in-out hover:border-green hover:text-brown"
       >
         <FaGooglePlusG />
-      </div>
+      </Link>
       <div className="border-2 border-light-gray rounded-full p-2 text-green hover:scale-125 duration-1000 ease-in-out hover:border-green hover:text-brown">
         <FaLinkedinIn />
       </div>
--- src/Routes/PublicRoutes/router.jsx
+++ src/Routes/PublicRoutes/router.jsx
@@ -14,6 +14,10 @@ export const router = createBrowserRouter([
         element: <Home/>,
       },
       {
+        path: "/home",
+        element: <Home/>,
+      },
+      {
         path: "/register",
         element: <Register/>,
       },
