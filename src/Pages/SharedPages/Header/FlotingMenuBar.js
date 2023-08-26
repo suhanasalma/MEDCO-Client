dab73d62c62c246d0bc5987a56d54a639f5ff78b
salma <suhanasalma800@gmail.com>
2023-08-27 00:53:54 +0600

    border added to floating menu bar

--- src/Pages/SharedPages/Header/FlotingMenuBar.js
+++ src/Pages/SharedPages/Header/FlotingMenuBar.js
@@ -21,7 +21,7 @@ const FlotingMenuBar = ({
 }) => {
   return (
     <section
-      className={`h-fit duration-1000 ease-in-out absolute z-40 w-full bg-green text-white  ${
+      className={`h-fit duration-1000 ease-in-out absolute z-40 w-full bg-green text-white border-b-2 border-white ${
         mainNavOpen ? "p-5 top-14 md:top-12 lg:top-20 " : "top-[-500px] "
       }`}
     >
