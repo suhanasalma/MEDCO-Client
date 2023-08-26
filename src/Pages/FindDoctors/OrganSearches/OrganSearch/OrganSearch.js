a9573e5dd322aab2b1317dea9cc805d0b2bf365d
salma <suhanasalma800@gmail.com>
2023-08-27 00:46:14 +0600

    OrganSearch design changed

--- src/Pages/FindDoctors/OrganSearches/OrganSearch/OrganSearch.js
+++ src/Pages/FindDoctors/OrganSearches/OrganSearch/OrganSearch.js
@@ -26,7 +26,7 @@ const OrganSearch = () => {
   };
 
   const handleBodyPartClick = (event) => {
-    setSelectDoctor('')
+    setSelectDoctor("");
     setOrgan(event);
     setVisibleHuman(false);
     setVisibleDoctors(false);
@@ -35,7 +35,7 @@ const OrganSearch = () => {
     setCurrentStep(2);
   };
   const handleOrganClick = (event) => {
-    setSelectDoctor('')
+    setSelectDoctor("");
     setSymptom(event);
     setVisibleOrgan(false);
     setVisibleHuman(false);
@@ -44,7 +44,7 @@ const OrganSearch = () => {
     setCurrentStep(3);
   };
   const handleSymptomsClick = (event) => {
-    setDoctorsAvailable(false)
+    setDoctorsAvailable(false);
     setSelectDoctor(event);
     setVisibleDoctors(true);
     setVisibleOrgan(false);
@@ -124,101 +124,107 @@ const OrganSearch = () => {
 
   return (
     <div className="p-2 w-full mx-auto mb-10">
-        <ProgressBar
-            stepsName={stepsName}
-            currentStep={currentStep}
-            progress={progress}
-            showSymptoms={showSymptoms}
-            showOrgans={showOrgans}
-            showBodys={showBodys}
-        />
-        <p className="text-center text-brown font-medium text-2xl hidden xl:block">
-            Interactive Symptom Checker{" "}
-        </p>
-        <div
-            className={`my-14 h-[440px] px-6 py-10 ${
-            visibleDoctors && "hidden xl:block"
-            }`}
-            style={{ boxShadow: " 0 .5rem 1rem rgba(0,0,0,.15)" }}>
-            <section className="flex items-end gap-5 h-full justify-between py-10">
-                <article
-                    className={`${
-                    visibleHuman ? "xl:w-auto" : "hidden xl:inline"
-                    }  h-full w-full xl:w-auto`}>
-                    <p className="text-center text-brown xl:text-green">
-                         Select rigion of the body:
-                    </p>
-                    <HumanBody
-                        organ={organ}
-                        handleBodyPartClick={handleBodyPartClick}
-                    />
-                </article>
-                <article
-                    className={`${
-                    visibleOrgan ? "" : "hidden xl:inline"
-                    } xl:inline w-full xl:w-auto h-full`}>
-                    <p className="text-center text-brown xl:text-green">
-                        Select affected organ:
-                    </p>
-                    <AffectedOrgans
-                    organ={organ}
-                    organsInfos={organsInfos}
-                    symptom={symptom}
-                    handleOrganClick={handleOrganClick}
-                    />
-                </article>
-                <article
-                    className={`${
-                    visibleSymptom ? "" : "hidden xl:inline"
-                    }  xl:inline h-full w-full xl:w-3/12`}>
-                    <p className="text-center text-brown xl:text-green xl:hidden">
-                        {symptom}
-                    </p>
-                    <p className="text-center text-brown xl:text-green">
-                        Select Symptom:
-                    </p>
-                    <SelectSymptoms
-                        selectDoctor={selectDoctor}
-                        handleSymptomsClick={handleSymptomsClick}
-                        Symptoms={Symptoms}
-                        symptom={symptom}
-                    />
-                </article>
-            </section>
-        </div>
-        <div
-            className={` ${
-            visibleDoctors ? "" : "hidden xl:block "
-            }  h-full w-full  mt-20 xl:mt-5`}>
-            <div className="w-full my-5 flex justify-center items-center">
-                {selectDoctor === "Others" && (
-                    <div className="w-5/6 sm:w-4/6 gap-10 space-y-6 xl:space-y-0 xl:flex items-center mx-auto">
-                        <CustomSelect
-                            setOpenSelectIndex={setOpenSelectIndex}
-                            isOpen={openSelectIndex === 0}
-                            toggleSelect={() => handleSelectToggle(0)}
-                            height={52}
-                            selectorDetails={dateSelector}
-                        />
-                        <CustomSelect
-                            setOpenSelectIndex={setOpenSelectIndex}
-                            isOpen={openSelectIndex === 1}
-                            toggleSelect={() => handleSelectToggle(1)}
-                            height={52}
-                            selectorDetails={timeSelector}
-                        />
-                        <button onClick={()=>setDoctorsAvailable(true)}
-                            className={` bg-green text-white text-md  px-4 py-1 mx-auto w-full rounded-md `} >
-                            Search Doctor
-                        </button>
-                    </div>
-                )}
+      <ProgressBar
+        stepsName={stepsName}
+        currentStep={currentStep}
+        progress={progress}
+        showSymptoms={showSymptoms}
+        showOrgans={showOrgans}
+        showBodys={showBodys}
+      />
+      <p className="text-center text-brown font-medium text-2xl hidden xl:block">
+        Interactive Symptom Checker{" "}
+      </p>
+      <div
+        className={`my-14 h-[440px] px-6 py-10 ${
+          visibleDoctors && "hidden xl:block"
+        }`}
+        style={{ boxShadow: " 0 .5rem 1rem rgba(0,0,0,.15)" }}
+      >
+        <section className="flex items-end gap-5 h-full justify-between py-10">
+          <article
+            className={`${
+              visibleHuman ? "xl:w-auto" : "hidden xl:inline"
+            }  h-full w-full xl:w-auto`}
+          >
+            <p className="text-center text-brown xl:text-green">
+              Select rigion of the body:
+            </p>
+            <HumanBody
+              organ={organ}
+              handleBodyPartClick={handleBodyPartClick}
+            />
+          </article>
+          <article
+            className={`${
+              visibleOrgan ? "" : "hidden xl:inline"
+            } xl:inline w-full xl:w-auto h-full`}
+          >
+            <p className="text-center text-brown xl:text-green">
+              Select affected organ:
+            </p>
+            <AffectedOrgans
+              organ={organ}
+              organsInfos={organsInfos}
+              symptom={symptom}
+              handleOrganClick={handleOrganClick}
+            />
+          </article>
+          <article
+            className={`${
+              visibleSymptom ? "" : "hidden xl:inline"
+            }  xl:inline h-full w-full xl:w-3/12`}
+          >
+            <p className="text-center text-brown xl:text-green xl:hidden">
+              {symptom}
+            </p>
+            <p className="text-center text-brown xl:text-green">
+              Select Symptom:
+            </p>
+            <SelectSymptoms
+              selectDoctor={selectDoctor}
+              handleSymptomsClick={handleSymptomsClick}
+              Symptoms={Symptoms}
+              symptom={symptom}
+            />
+          </article>
+        </section>
+      </div>
+      <div
+        className={` ${
+          visibleDoctors ? "" : "hidden xl:block "
+        }  h-full w-full  mt-20 xl:mt-5`}
+      >
+        {selectDoctor === "Others" && (
+          <div className="w-full my-5 flex justify-center items-center mb-14">
+            <div className="w-5/6 sm:w-4/6 gap-10 space-y-6 xl:space-y-0 xl:flex items-center mx-auto">
+              <CustomSelect
+                setOpenSelectIndex={setOpenSelectIndex}
+                isOpen={openSelectIndex === 0}
+                toggleSelect={() => handleSelectToggle(0)}
+                height={52}
+                selectorDetails={dateSelector}
+              />
+              <CustomSelect
+                setOpenSelectIndex={setOpenSelectIndex}
+                isOpen={openSelectIndex === 1}
+                toggleSelect={() => handleSelectToggle(1)}
+                height={52}
+                selectorDetails={timeSelector}
+              />
+              <button
+                onClick={() => setDoctorsAvailable(true)}
+                className={` bg-green text-white text-md  px-4 py-1 mx-auto w-full rounded-md `}
+              >
+                Search Doctor
+              </button>
             </div>
-            {(selectDoctor && selectDoctor !== "Others") || doctorsAvaialble && 
-            
-            <SelectDoctor />
-            }
-        </div>
+          </div>
+        )}
+        {((selectDoctor && selectDoctor !== "Others") || doctorsAvaialble) && (
+          <SelectDoctor />
+        )}
+      </div>
     </div>
   );
 };
