f9b64cef258a6d2f9e8a4ee4aa92d3d94f560848
suhana salma <salma@parallaxlogic.net>
2023-08-27 17:57:27 +0600

    ClinicAndCenters Carousel added

--- src/Pages/ClinicAndCenters/ClinicAndCentersContainer/ClinicAndCenters.js
+++ src/Pages/ClinicAndCenters/ClinicAndCentersContainer/ClinicAndCenters.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Carousel from '../../Home/Carousel/Carousel';
+import BreadCrumbs from '../../../Components/BreadCrumbs/BreadCrumbs';
+import Title from '../../../Components/Title/Title';
 
 const ClinicAndCenters = () => {
     return (
         <div>
-            <p>this is clicnic and centers</p>
+            <div>
+            <Carousel/>
+            <BreadCrumbs  breadcrumbs={[
+          { title: "Home", link: "/home" },
+          {
+            title: "Clinics and Centers",
+            active: true,
+            link: "/ClincAndCenter",
+          },
+        ]}/>
+        </div>
+        <div className='my-10'>
+            <Title design='title text-center' title='Clinic and Center'/>
+        </div>
         </div>
     );
 };
